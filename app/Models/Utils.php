56e373e00324adb144a23c8ecbc86492380a50b2
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-31 17:02:30 +0300

    storage

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -88,7 +88,7 @@ class Utils
                 $file_name = time() . "-" . rand(100000, 1000000) . "." . $ext;
                 $root_path = $_SERVER['DOCUMENT_ROOT'];
 
-                $destination = $root_path.'/ict4farmers/public/storage/images/' . $file_name;
+                $destination = '/public/storage/' . $file_name;
 
                 $res = move_uploaded_file($file['tmp_name'], $destination);
                 if (!$res) {
