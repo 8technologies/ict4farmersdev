3decb6bf1f7947b51b0d6b7e4fd79070babde4b4
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-31 13:46:09 +0300

    animal_id

--- app/Http/Controllers/ApiShopController.php
+++ app/Http/Controllers/ApiShopController.php
@@ -226,8 +226,7 @@ class ApiShopController extends Controller
         $pro->name = $r->name;
         $pro->feature_photo = 'no_image.jpg';
         $pro->description = $r->description;
-        $pro->price_1 = $r->price_1;
-        $pro->animal_id = $r->animal_id;
+        $pro->price_1 = $r->price_1; 
         $pro->price_2 = $r->price_2;
         $pro->local_id = $r->id;
         $pro->summary = $r->data;
