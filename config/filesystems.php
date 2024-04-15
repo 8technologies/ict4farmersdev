91c78f84915f4ae2997a8cdbd0141204f2ed4a8b
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-04-15 11:20:58 +0300

    as

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -55,8 +55,8 @@ return [
         ],
         'admin' => [
             'driver' => 'local',
-            'root' => base_path('public/storage'),
-            'url' => env('APP_URL').'/public/storage',
+            'root' => str_replace(base_path('public/storage'), 'ict4farmers/', ''),
+            'url' => env('APP_URL') . '/public/storage',
             'visibility' => 'public',
         ],
     ],
