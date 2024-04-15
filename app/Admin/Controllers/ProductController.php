59369005573f6831e5b92591f713b5d94aaf40af
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-04-15 10:11:14 +0300

    as

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -192,9 +192,11 @@ class ProductController extends AdminController
 
         $form = new Form(new Product());
         $form->text('name', __('Name'));
-        $form->image('feature_photo', 'Main Image')->uniqid();
-        $form->hasMany('images', 'images', function ($f) { 
+        $form->image('feature_photo', __('Main image'))->uniqueName();
+        
+        $form->hasMany('images', 'images', function ($f) {
             $u = Admin::user();
+            $f->image('src', 'Image')->uniqueName();
             $f->hidden('user_id')->default($u->id);
             $f->hidden('parent_endpoint')->default('Product');
             $f->hidden('note')->default('Product');
